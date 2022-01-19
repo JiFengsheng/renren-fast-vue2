afd1bf801531587b070fa8366457378806b41824
JiFengsheng <1923292724@qq.com>
2022-01-19 10:49:01 +0800

    update

---
+++ src/views/modules/product/test.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+
+  components: {},
+
+  methods: {
+      test(){
+      }
+  }
+}
+</script>
+
+<style scoped>
+</style>
